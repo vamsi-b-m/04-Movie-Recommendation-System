0bdc85796b1f5f1e896e30653a30f6b206a94e00
vamsi-b-m <vamsi.b.x@gmail.com>
2024-01-27 21:37:59 +0530

    Created the Project Structure

---
+++ main.py
@@ -0,0 +1,7 @@
+# 1. Data Collection
+# 2. Data Ingestion
+# 3. Data Validation
+# 4. Data Visualization
+# 5. Model Initialization
+# 6. Model Evaluation
+# 7. Model Push
---
+++ src/constant/__init__.py
@@ -0,0 +1,8 @@
+# Data Ingestion Constants
+DATA_INGESTION_CONFIG_KEY = "data_ingestion_config"
+DATA_INGESTION_DATASET_DOWNLOAD_URL_KEY = "dataset_download_url"
+DATA_INGESTION_TGZ_DATA_DIR_KEY = "tgz_data_dir"
+DATA_INGESTION_RAW_DATA_DIR_KEY = "raw_data_dir"
+DATA_INGESTION_PROCESSED_DATA_DIR = "processed_data_dir"
+DATA_INGESTION_TRAIN_DATA_DIR = "train_data"
+DATA_INGESTION_TEST_DATA_DIR = "test_data"
